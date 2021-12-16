d57c423d81f0380e55c1c13d6e4b961a1917e8bf
Anatolii Tretiakov <tretyakovanatoly@gmail.com>
2021-12-16 15:41:37 +0100

    custom reset password form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import { Auth } from 'aws-amplify';
+import { AuthState } from '@aws-amplify/ui-components';
+import { Authenticator, SignIn, SignUp, ConfirmSignUp, Greetings, ForgotPassword  } from 'aws-amplify-react';
+import '@aws-amplify/ui/dist/style.css';
+import CustomForgotPassword from './Components/Login/CustomForgotPassword';
+
 
 function App() {
+  console.log(AuthState)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Authenticator hide={
+          [
+              ForgotPassword
+          ]
+      }>
+        <CustomForgotPassword override={'ForgotPassword'}/>
+      </Authenticator>
+
   );
 }
 
-export default App;
+export default App
